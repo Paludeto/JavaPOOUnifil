dca890e37e95942d6ca84263bc8f512197e4baee
Paludeto <gpalu54@gmail.com>
2021-10-11 02:47:40 -0300

    Trabalho AtividadeTryCatchArray adicionado.

---
+++ AtividadeTryCatchArray/src/TestaUsaArray.java
@@ -0,0 +1,51 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+public class TestaUsaArray{
+    public static void main(String args[]){
+        Scanner teclado = new Scanner(System.in);
+        int a[] = new int[5];
+        int b[] = new int[5];
+        int c[] = new int[5];
+        int opcao = 0;
+        UsaArray usa = new UsaArray();
+        while (opcao != 9){
+            System.out.println("1-atualiza array A");
+            System.out.println("2-atualiza array B");
+            System.out.println("3-mostra array A");
+            System.out.println("4-mostra array B");
+            System.out.println("5-mostra array C");
+            System.out.println("6-gerar array C divisão");
+            System.out.println("9-fim");
+//3)implemente try..catch
+            try {
+                opcao = teclado.nextInt();
+            } catch (InputMismatchException e) {
+                System.out.println("Erro, escolha uma opção válida.");
+                teclado.nextLine();
+            }
+
+            switch(opcao){
+                case 1 : //4) invocar o método carregaArray()
+                    usa.carregaArray(a);
+                    break;
+                case 2 : //5) invocar o método carregaArray()
+                    usa.carregaArray(b);
+                    break;
+                case 3 : //6) invocar o método mostraArray() e mostrar os números do array A
+                    usa.mostraArray(a);
+                    break;
+                case 4 : //7) invocar o método mostraArray() e mostrar os números do array B
+                    usa.mostraArray(b);
+                    break;
+                case 5 : //8) invocar o método mostraArray() e mostrar os números do array C
+                    usa.mostraArray(c);
+                    break;
+                case 6 : //9) invocar o método geraArrayDivisao() para carregar o array C
+                    c = usa.geraArrayDivisao(a, b);
+                    break;
+                default:
+                    System.out.println("Opção inválida");
+            }
+        }
+    }
+}
---
+++ AtividadeTryCatchArray/src/UsaArray.java
@@ -0,0 +1,46 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class UsaArray{
+
+    private Scanner teclado;
+
+    public UsaArray(){
+        teclado = new Scanner(System.in);
+    }
+
+    public void carregaArray(int[] arr) { //1) implemente try..catch
+        for(int i=0; i<arr.length; i++){
+            System.out.println("Informe Numero: ");
+            try {
+                arr[i] = teclado.nextInt();
+            } catch (InputMismatchException e) {
+                System.out.println("Erro, teclado só aceita inteiros.");
+                teclado.nextLine();
+            }
+        }
+    }
+
+    public String mostraArray(int arr[]) {
+        String saida="";
+        for(int i=0; i<arr.length; i++){
+            saida = saida + arr[i] + '\n';
+        }
+        return saida;
+    }
+
+    public int[] geraArrayDivisao(int arrA[],
+                                  int arrB[]) { //2) implemente try..catch
+        int aux[] = new int[arrA.length];
+        try {
+            for (int i = 0; i < arrA.length; i++) {
+                aux[i] = arrA[i] / arrB[i];
+            }
+        } catch (ArithmeticException e) {
+            System.out.println("Impossível realizar a divisão por 0.");
+            teclado.nextLine();
+        }
+        return aux;
+    }
+
+}

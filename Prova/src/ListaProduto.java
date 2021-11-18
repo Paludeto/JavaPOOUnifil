fa212d8cf5a7c40294c56490a6f49bf8d312e97f
Paludeto <gpalu54@gmail.com>
2021-11-18 00:46:58 -0300

    Métodos pedidos adicionados.

--- Prova/src/ListaProduto.java
+++ Prova/src/ListaProduto.java
@@ -1,29 +1,92 @@
 import java.lang.reflect.Array;
-import java.util.ArrayList;
+import java.util.*;
 
 public class ListaProduto {
-
     ArrayList<Produto> lista;
 
     public ListaProduto() {
-        this.lista = new ArrayList<Produto>();
+        lista = new ArrayList<Produto>();
     }
 
     public void addProduto(Produto prod) {
         lista.add(prod);
     }
 
-    public ArrayList<String> mostraProdutos() {
-        Produto prod;
-        ArrayList<String> array = new ArrayList<String>();
+    //1) altere o metodo para não retornar no ArrayList<String> descrições duplicadas
+//lista = notebook, notebook, teclado, .... , notebook
+//listaInput = mesa, cadeira, notebook, teclado
+//saida = notebook, notebook, teclado ... , notebook <-- errado
+//saida = notebook, teclado <-- certo
+    public ArrayList<String> getProdutoDuplicada(ArrayList<Produto> listaInput) {
 
-        for(int i = 0; i < lista.size(); i ++) {
-            prod = lista.get(i);
-            array.add(prod.getDescricao());
+        ArrayList<String> saida = new ArrayList<>();
+
+        for (Produto prod : lista) {
+            if (!saida.contains(prod.getDescricao())) {
+                saida.add(prod.getDescricao());
+            }
         }
 
-        return array;
+        return saida;
+
     }
 
+    //2) crie um método que retorne a descrição de todos os produtos que estão nas duas listas
+//atributo e parâmetro
+//porém não deve retornar no ArrayList<String> as descrições duplicadas
+    public ArrayList<String> retornaDescricoesEmComum(ArrayList<Produto> listaProduto) {
+
+        ArrayList<String> saida = new ArrayList<>();
+
+        for (Produto prod : lista) {
+            int i = 0;
+            while (i < listaProduto.size()) {
+                if (Objects.equals(prod.getDescricao(), listaProduto.get(i).getDescricao())) {
+                    saida.add(prod.getDescricao());
+                }
+                i++;
+            }
+        }
+
+        for (int i = 0; i < saida.size(); i++) {
+            for (int j = 0; j < saida.size(); j++) {
+                if (Objects.equals(saida.get(i), saida.get(j))) {
+                    saida.remove(i);
+                }
+            }
+        }
+
+        return saida;
+
+    }
+
+//3) crie um método que retorne a descrição de todos os produtos que estão no atributo lista ArrayList<Produto>
+//e que não estão na lista do parâmetro listaInput. Não armazenar descrições duplicadas no ArrayList<String>
+//lista = notebook, notebook, teclado, notebook, teclado
+//listaInput = mesa, cadeira, notebook
+//saida = teclado <-- certo
+    public ArrayList<String> retornaDescricoesDiferentes(ArrayList<Produto> listaInput) {
+
+        ArrayList<String> saida = new ArrayList<>();
+
+        for (int i = 0; i < listaInput.size(); i++) {
+            for (int j = 0; j < lista.size(); j++) {
+                if (!Objects.equals(lista.get(i).getDescricao(), listaInput.get(i).getDescricao())) {
+                    saida.add(lista.get(i).getDescricao());
+                }
+            }
+        }
+
+        for (int i = 0; i < saida.size(); i++) {
+            for (int j = 0; j < saida.size(); j++) {
+                if (Objects.equals(saida.get(i), saida.get(j))) {
+                    saida.remove(i);
+                }
+            }
+        }
+
+        return saida;
+
+    }
 
 }
--- Prova/src/TesteProduto.java
+++ Prova/src/TesteProduto.java
@@ -5,33 +5,29 @@ public class TesteProduto {
     public static void main(String[] args) throws DescricaoNulaException, SaldoInsuficienteException {
         ListaProduto lp = new ListaProduto();
         Produto prod1 = new Produto("Playstation");
-        Produto prod2 = new Produto("Máquina de lavar");
+        Produto prod2 = new Produto("Playstation");
         Produto prod3 = new Produto("Mouse");
         Produto prod4 = new Produto("Teclado");
+        Produto prod5 = new Produto("Mouse");
+        Produto prod6 = new Produto("Outra Coisa");
+
         lp.addProduto(prod1);
         lp.addProduto(prod2);
         lp.addProduto(prod3);
         lp.addProduto(prod4);
+        lp.addProduto(prod5);
 
-        try {
-            prod1.setDescricao(null);
-        } catch(DescricaoNulaException e) {
-            e.printStackTrace();
-        }
+        ArrayList<Produto> novoArray = new ArrayList<>();
+        novoArray.add(prod1);
+        novoArray.add(prod2);
+        novoArray.add(prod3);
+        novoArray.add(prod6);
 
-        try {
-            prod1.saidaProduto(2500);
-        } catch(SaldoInsuficienteException e) {
-            e.printStackTrace();
-        }
 
-        System.out.println(lp.mostraProdutos());
+        System.out.println(lp.getProdutoDuplicada(lp.lista));
+        System.out.println(lp.retornaDescricoesEmComum(novoArray));
+        System.out.println(lp.retornaDescricoesDiferentes(novoArray));
 
-        prod1.setDescricao("Geladeira");
-        prod1.saidaProduto(0);
-
-        System.out.println(lp.mostraProdutos());
     }
-
 
 }
